68709245bc18efd8b0cdbb8d0b2035cb881f8a3d
0youn <younes@MacBook-Y.local>
2025-11-26 16:26:16 +0100

    exercices 1 et 3 tp1

---
+++ TP1/exo1.py
@@ -0,0 +1,9 @@
+nom = "TRUMP"
+prenom = "Donald"
+maths = 2
+anglais = 18
+info = 19
+promotion = 2025
+m = f"L’étudiant {prenom} {nom} de la promotion {promotion} a une moyenne de {(maths + anglais + info) / 3} sur 20."
+
+print(m)
---
+++ TP1/exo3.py
@@ -0,0 +1,10 @@
+# Déclaration des variables
+jour = 26
+heure = 16
+minute = 25
+
+# Calcul du nombre de minutes écoulées depuis le début du mois
+minutes_ecoulees = ((jour - 1) * 24 * 60) + (heure * 60) + minute
+
+# Affichage du résultat
+print("Nombre de minutes écoulées depuis le début du mois :", minutes_ecoulees)
